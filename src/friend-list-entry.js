fd05da2df18b5a790054f871014a391a91ec18b5
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2019-07-30 21:40:22 -0600

    finished project

---
+++ src/friend-list-entry.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function(props) {
+  // Data that we'll need:
+  // - first name
+  // - last name
+  // - status
+  // - id: id
+  // - image: img
+
+  const { id, img, first_name, last_name, status } = props.entry;
+  return (
+    <div className="friend-list-card">
+      <div className="friend-info-image">
+        <img src={img} />
+        <div className="friend-info">
+          {first_name + " " + last_name}
+          <div>{status}</div>
+          <div />
+        </div>
+      </div>
+      <Link to={`/friend/${id}}`} className="friend-detail-button">
+        Link
+      </Link>
+    </div>
+  );
+}
